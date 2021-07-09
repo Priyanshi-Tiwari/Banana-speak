101cf3195278c386f237d4b54f9b9a29ddedb18c
Priyanshi-Tiwari <71331989+Priyanshi-Tiwari@users.noreply.github.com>
2021-07-09 10:57:25 +0530

    wired fetch call

--- app.js
+++ app.js
@@ -4,8 +4,26 @@ var txtinput= document.querySelector("#txt-input");
 
 var outputDiv=document.querySelector("#output-div");
 
+var url="https://api.funtranslations.com/translate/minion.json"
+ 
+function constructURL(input){
+    return url+"?"+"text="+input
+}
+
+function errorHandler(error){
+    console.log("error occured "+ error)
+}
 function clickHandler() {
-   outputDiv.innerText="akakak"+ txtinput.value
+    var textinput=txtinput.value
+    
+        fetch(constructURL(textinput))
+        .then(response=>response.json())
+        .then(json=>{
+        outputDiv.innerText=json.contents.translated
+        })
+        .catch(errorHandler)
+    
+   
 };
 
 
